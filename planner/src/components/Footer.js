a8d504e508e435680aa06c7a1665599eab1f0cf0
MacTroWSB <mactro96@o2.pl>
2024-05-09 14:02:25 +0200

    Footer fixes

--- planner/src/components/Footer.js
+++ planner/src/components/Footer.js
@@ -7,15 +7,6 @@ function Footer() {
       <div className="footer-content">
         <p>© 2023-2024  Maciej Trojan. All rights reserved.</p>
         <div className="social-media">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-instagram"></i>
-          </a>
         </div>
       </div>
     </footer>
